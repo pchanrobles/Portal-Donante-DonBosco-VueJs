8451b2f116ca76f32ba41bb3b05a35ce16fec5b8
rayosur <gonzalez.garrido@hotmail.com>
2021-09-20 15:20:24 +0200

    login

---
+++ src/layout/Login.vue
@@ -0,0 +1,110 @@
+<template>
+
+  <div id="app">
+  <v-app :class="{ 'pa-3': $vuetify.breakpoint.smAndUp }" :dark="darkTheme" id="inspire">
+    <v-container>
+      <v-layout wrap>
+        <v-flex sm12 md6 offset-md3>
+          <v-card elevation="4" light tag="section">
+            <v-card-title>
+              <v-layout align-center justify-space-between>
+                <h3 class="headline">
+                  Portal Donante
+                </h3>
+               
+              </v-layout>
+            </v-card-title>
+            <v-divider></v-divider>
+            <v-card-text>
+              <p>Inicie sesión</p>
+              <v-form>
+                <v-text-field
+                              outline
+                              label="Username"
+                              type="text"
+                              v-model="username"></v-text-field>
+                <v-text-field
+                              outline
+                              hide-details
+                              label="Password"
+                              type="password"
+                              v-model="password"></v-text-field>
+              </v-form>
+            </v-card-text>
+            <v-divider></v-divider>
+            <v-card-actions :class="{ 'pa-3': $vuetify.breakpoint.smAndUp }">
+                <div style="display:flex; flex-direction:column; align-items:center;">
+                    <div style="display:flex; justify-content:space-between; width:100%; align-items:center;">
+
+                        <v-checkbox label="Recordar contraseña"></v-checkbox>
+
+                        <v-btn  color="info" :large="$vuetify.breakpoint.smAndUp">
+                            Login
+                        </v-btn>
+                    </div>
+
+                    <v-spacer></v-spacer>
+                    
+                    <v-btn color="info" text>
+                        ¿Se te olvidó tu contraseña?
+                    </v-btn>
+                </div>
+            </v-card-actions>
+          </v-card>
+        </v-flex>
+        <v-flex sm12 md6 offset-md3>
+          <v-layout align-center justify-space-between>
+          </v-layout>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </v-app>
+</div>
+                                
+
+</template>
+
+<script>
+import AuthService from "@/services/AuthService.js";
+
+export default {
+data () {
+    return {
+      darkTheme: true,
+      platformName: 'Platform name',
+      password: null,
+      username: null
+    }
+  },
+  methods: {
+    login(){
+      let data = {
+        email: this.email,
+        password: this.password
+      }
+
+      AuthService.login(data)
+      .then(res =>{
+        console.log(res)
+        localStorage.setItem('TokenFIRE',res.data.token)
+        localStorage.setItem('Usuario',res.data.Usuario.name)
+        })
+        .catch((errors) => {
+          console.log(errors);
+        })
+    }
+  }
+
+  }
+   
+
+</script>
+
+<style>
+
+.v-card__actions {
+    display: block;
+ }
+ 
+
+</style>
