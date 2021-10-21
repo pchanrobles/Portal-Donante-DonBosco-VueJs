f45bb78c9038ee5955663bdc1c876ff310c3e34e
rayosur <gonzalez.garrido@hotmail.com>
2021-10-21 14:12:42 +0200

    admin

---
+++ src/layout/DashboardAdmin.vue
@@ -0,0 +1,146 @@
+<template>
+  <v-app>
+    <v-navigation-drawer
+      style="background-color: #8b0000"
+      v-model="drawer"
+      absolute
+      temporary
+      app
+    >
+      <v-list nav dense>
+        <v-list-item-group
+          v-model="group"
+          active-class="#DC001B --text text--accent-4"
+        >
+          <v-list-item>
+            <v-list-item-icon> </v-list-item-icon>
+            <v-list-item-title>
+              <h3 style="color: white; font-family: MONTSERRAT">
+                Admin
+              </h3></v-list-item-title
+            >
+          </v-list-item>
+
+          <v-main></v-main>
+          <v-list-item>
+            <v-list-item-icon>
+              <i
+                class="fas fa-house-user"
+                style="font-size: 25px; color:white"
+              ></i>
+            </v-list-item-icon>
+            <a href="/" style="color: white; font-family: MONTSERRAT">INICIO</a>
+          </v-list-item>
+
+       
+
+          <v-list-item>
+            <v-list-item-icon>
+              <i
+                class="fas fa-bullhorn"
+                style="font-size: 22px; color: #ffe900"
+              ></i>
+            </v-list-item-icon>
+            <a href="ComunicadosAdmin" style="color: white; font-family: MONTSERRAT"
+              >Comunicados</a
+            >
+          </v-list-item>
+
+           <v-list-item>
+            <v-list-item-icon>
+              <i class="far fa-user" style="font-size: 22px; color: cyan"></i>
+            </v-list-item-icon>
+            <a href="BuscarDonantes" style="color: white; font-family: MONTSERRAT"
+              >&nbsp;Perfil de Donantes</a
+            >
+
+          </v-list-item>
+
+           <v-list-item>
+            <v-list-item-icon>
+              <i class="far fa-bell" style="font-size: 22px; color: orange"></i>
+            </v-list-item-icon>
+            <a
+              href="ComunicadosAdmin"
+              style="color: white; font-family: MONTSERRAT"
+              >&nbsp;Notificaciones</a
+            >
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-icon>
+              <i
+                class="fas fa-question"
+                style="font-size: 22px; color: blue"
+              ></i>
+            </v-list-item-icon>
+            <a href="Peticion" style="color: white; font-family: MONTSERRAT"
+              >&nbsp;Peticiones</a
+            >
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-icon>
+              <i
+                class="far fa-times-circle"
+                style="font-size: 22px; color: grey"
+              ></i>
+            </v-list-item-icon>
+            <a href="Login" style="color: white; font-family: MONTSERRAT"
+              >&nbsp;Cerrar Sesion</a
+            >
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar color="#DC001B" app>
+      <v-app-bar-nav-icon @click="drawer = true">
+        <i
+          class="fas fa-align-justify"
+          style="font-size: 30px; color: white"
+        ></i>
+      </v-app-bar-nav-icon>
+
+      <v-toolbar-title style="font-family: MONTSERRAT; color: white"
+        >&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;👨‍⚖️Administrador</v-toolbar-title
+      >
+    </v-app-bar>
+
+    <!-- Sizes your content based upon application components -->
+    <v-main>
+      <!-- Provides the application the proper gutter -->
+      <v-container fluid>
+        <!-- If using vue-router -->
+        <router-view></router-view>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+Vue.use(Vuetify);
+
+import Vue from "vue";
+import Vuetify from "vuetify/lib";
+import News from "../components/News.vue";
+
+export default {
+  components: {
+    News,
+  },
+
+  data: () => ({
+    drawer: false,
+    group: null,
+  }),
+};
+</script>
+
+<style>
+a:link,
+a:visited,
+a:active {
+  text-decoration: none;
+}
+</style>
---
+++ src/views/BuscarDonantes.vue
@@ -0,0 +1,47 @@
+<template>
+<v-app>
+<div class="box">
+  <div class="container-4">
+    <input type="search" id="search" placeholder="Buscar Donantes por DNI..." />
+    <button class="icon"><i class="fa fa-search"></i></button>
+    
+  </div>
+  <br>
+  <br>
+
+  <Comunicados></Comunicados> <br> <Comunicados></Comunicados>
+   
+</div>
+</v-app>
+</template>
+<script>
+
+import Comunicados  from '../components/Comunicados.vue'
+
+export default{
+    components:{
+        Comunicados
+    }
+  
+}
+</script>
+
+<style scoped>
+
+.container-4{
+    color:red;
+    display:flex;
+    justify-content: center;
+    margin-top:30px;
+    
+}
+.icon{
+    color:red
+}
+.buscador{
+    color:red;
+}
+.formulario{
+    color:blue;
+}
+</style>
